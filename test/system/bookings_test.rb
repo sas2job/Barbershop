bbe9d5eb10877ab3f1316a8108a7e170eaa5489a
Alex Smirnov <sas2job@gmail.com>
2026-08-29 22:04:50 +0300

    test(ui): cover dynamic booking slots

---
+++ test/system/bookings_test.rb
@@ -0,0 +1,30 @@
+require "application_system_test_case"
+
+class BookingsTest < ApplicationSystemTestCase
+  setup do
+    @service = Service.create!(
+      name: "Мужская стрижка",
+      category: :men,
+      duration_minutes: 45,
+      price_from_cents: 55_000,
+      price_to_cents: 55_000
+    )
+    @date = Date.current + 7
+    @next_date = @date + 1
+  end
+
+  test "refreshes available slots after changing the date" do
+    visit new_booking_path(service_id: @service.id, date: @date.iso8601)
+
+    date_field = find("[data-booking-date]")
+    execute_script(
+      "arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('change', { bubbles: true }));",
+      date_field,
+      @next_date.iso8601
+    )
+
+    expected_slot = BookingSchedule.slots_for(@next_date).first
+    assert_selector "select[data-booking-slots] option[value='#{expected_slot.iso8601}']", wait: 5
+    assert_equal @next_date.iso8601, find("[data-booking-date-value]", visible: false).value
+  end
+end
