1f56b329007bd6c8f63692e8b8b4d16b475222ec
Alex Smirnov <sas2job@gmail.com>
2026-08-29 22:40:17 +0300

    test(ui): cover rescheduling slots

--- test/system/bookings_test.rb
+++ test/system/bookings_test.rb
@@ -27,4 +27,26 @@ class BookingsTest < ApplicationSystemTestCase
     assert_selector "select[data-booking-slots] option[value='#{expected_slot.iso8601}']", wait: 5
     assert_equal @next_date.iso8601, find("[data-booking-date-value]", visible: false).value
   end
+
+  test "refreshes rescheduling slots after changing the date" do
+    booking = Booking.reserve!(
+      service: @service,
+      starts_at: BookingSchedule.slots_for(@date).first,
+      client_name: "Клиент",
+      phone_number: "8-000-000-00-00"
+    )
+
+    visit reschedule_booking_path(booking.public_token, date: @date.iso8601)
+
+    date_field = find("[data-booking-date]")
+    execute_script(
+      "arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('change', { bubbles: true }));",
+      date_field,
+      @next_date.iso8601
+    )
+
+    expected_slot = BookingSchedule.slots_for(@next_date).first
+    assert_selector "select[data-booking-slots] option[value='#{expected_slot.iso8601}']", wait: 5
+    assert_equal @next_date.iso8601, find("[data-booking-date-value]", visible: false).value
+  end
 end
